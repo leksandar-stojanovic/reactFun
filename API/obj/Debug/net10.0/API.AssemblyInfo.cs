a43fcc487f9b6c0a401b6d0d4fe0a542c97187c0
leksandar-stojanovic <astojanovic@jobadder.com>
2026-01-02 12:30:48 +1100

    Added DbInitializer with dummy data ready to go.

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a34cdac69ab19061aa6f9e6406851aa771a7a13f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4666d1b3e731a9241f22cf4f450b32e821efb3e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
